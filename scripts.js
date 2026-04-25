d48dac3449445eaec12105988fb01c955563bc28
Kirizakuga <poisonstone07@gmail.com>
2026-04-26 03:18:11 +0700

    adding some js and a popup, thats sucks, will fix it later

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
-// Comment
+const popup = document.getElementById("popup")
+const openBtn = document.getElementById("openBtn")
+const closeBtn = document.getElementById("clsBtn")
+
+openBtn.onclick = function(){
+    popup.style.display = "block";
+}
+
+closeBtn.onclick = function(){
+    popup.style.display = "none";
+}
+
+window.onclick = function(event){
+    if (event.target == popup){
+        popup.style.display = "none";
+    }
+}
