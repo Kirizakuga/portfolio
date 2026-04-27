a8d5e729ac51e9eb2acda1fc8ee51c5acb6712d7
Kirizakuga <poisonstone07@gmail.com>
2026-04-28 00:40:27 +0700

    fix the bugs that make droplist cannot close properly

--- scripts.js
+++ scripts.js
@@ -7,13 +7,6 @@ openBtn.onclick = function(event) {
     popup.style.display = "block";
 };
 
-// 2. Close the popup with [X]
-closeBtn.onclick = function(event) {
-    // This is the magic line! It stops the click from hitting the openBtn underneath it.
-    event.stopPropagation();
-    popup.style.display = "none";
-};
-
 // 3. Close the popup when clicking outside of it
 window.onclick = function(event) {
     // If the popup is open AND the user clicks completely outside the 'openBtn' container...
